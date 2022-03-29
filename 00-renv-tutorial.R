79256cd148f7599d08a6433f94c6a2a9ede14fb3
Meltem Odabas <meltem.odabas.mo@gmail.com>
2022-03-29 14:28:51 -0400

    Add webpage

--- 00-renv-tutorial.R
+++ 00-renv-tutorial.R
@@ -10,3 +10,8 @@ renv::init()
 
 #after loading new packages, take snapshots to update renv
 renv::snapshot()
+
+#once you open the project on a new computer, load the packages:
+renv::restore()
+
+#for more detail, visit: https://rstudio.github.io/renv/articles/renv.html
